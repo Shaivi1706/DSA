66d9269eadb9da3b7741c704920f9c0454035057
Shaivi Jain <shaivijainn@gmail.com>
2025-06-06 17:42:06 +0530

    Update LowestCommonAncestor.cpp

--- Binary Search Trees/LowestCommonAncestor.cpp
+++ Binary Search Trees/LowestCommonAncestor.cpp
@@ -11,3 +11,19 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
         return root;
     }
+
+________________________________________________________________________________
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return nullptr;
+
+        if(root->val>p->val && root->val>q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+
+        if(root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+
+        return root;
+    }
