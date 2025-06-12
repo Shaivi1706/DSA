481139673459a89938cde94643b17d87fdae565c
Shaivi Jain <shaivicseai@gmail.com>
2025-06-13 00:39:04 +0530

    Update NextGreaterElement1.cpp

--- Stacks/NextGreaterElement1.cpp
+++ Stacks/NextGreaterElement1.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>m;
-        stack<int>s;
-        int n=nums2.size();
-        s.push(nums2[n-1]);
-        m[nums2[n-1]]=-1;
-        for(int i=nums2.size()-2;i>=0;i--)
-        {
-            if(!s.empty())
-            {
-                int ma=INT_MIN;
-                cout<<s.top()<<endl;
-                while(!s.empty()&&s.top()<=nums2[i])
-                {
-                    s.pop();
-                    if(s.empty())break;
-                }
-                if(s.empty())m[nums2[i]]=-1;
-                else
-                m[nums2[i]]=max(s.top(),-11);
-            }
+        stack<int> s;
+        stack<int> temp;
+
+        for(int i=0; i<nums2.size(); i++) {
             s.push(nums2[i]);
         }
-        for(int i=0;i<nums1.size();i++)
-        {
-            nums1[i]=m[nums1[i]];
+
+        vector<int> ans(nums1.size());
+
+        for(int i=0; i<nums1.size(); i++) {
+            int max=-1;
+            temp=s;
+
+            while(temp.top()!=nums1[i]) {
+                if(temp.top()>nums1[i]) {
+                    max=temp.top();
+                }
+                temp.pop();
+            }
+
+            ans[i]=max;
         }
-        return nums1;
+
+        return ans;
     }
 };
