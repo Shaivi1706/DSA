9356933d75c3eb0e682691b44912c1c120d8428a
Shaivi Jain <shaivicseai@gmail.com>
2025-06-21 20:04:08 +0530

    Update PeakIn2d.cpp

--- BinarySearch/PeakIn2d.cpp
+++ BinarySearch/PeakIn2d.cpp
@@ -16,3 +16,41 @@ public:
         return {-1, -1};
     }
 };
+
+_______________________________________________________________________________
+
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Finding max in mid column
+            int maxRow = 0;
+            for (int i = 0; i < m; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
+                }
+            }
+
+            // Check neighbors
+            bool leftOK = (mid == 0 || mat[maxRow][mid] > mat[maxRow][mid - 1]);
+            bool rightOK = (mid == n - 1 || mat[maxRow][mid] > mat[maxRow][mid + 1]);
+
+            if (leftOK && rightOK) {
+                return {maxRow, mid};
+            } else if (mid > 0 && mat[maxRow][mid - 1] > mat[maxRow][mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return {-1, -1};
+    }
+};
